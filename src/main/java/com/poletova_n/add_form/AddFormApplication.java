e6e3eab3cc3cecaf6b8838e2ae77ae7b2608b137
Nadia Poletova <poletova-n@mail.ru>
2018-04-09 20:12:29 +0300

    htmlcss

---
+++ src/main/java/com/poletova_n/add_form/AddFormApplication.java
@@ -0,0 +1,12 @@
+package com.poletova_n.add_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddFormApplication.class, args);
+	}
+}
---
+++ src/test/java/com/poletova_n/add_form/AddFormApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poletova_n.add_form;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddFormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
